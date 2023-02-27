a312d290ecce4c56e5c906746169b7c2cedddcbd
smhrd <smhrd@DESKTOP-SFP78Q5>
2023-02-27 16:19:34 +0900

    2023-02-27 16:19 김원경 Create Project

---
+++ ESC/src/main/java/com/smhrd/EscApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscApplication.class, args);
+	}
+
+}
