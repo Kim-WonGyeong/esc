f7d86c6e605e89c09975de51e1b2dd8e462284b5
haexung <5un9.hae@gmail.com>
2023-03-13 17:18:30 +0900

    2023-03-13 17:17 박해성
    1. test용 문진 페이지, 결과 페이지 추가
    2. radio 타입으로 문항 내용 전달 >> 피부타입 진단
    >> 결과 저장 >> 출력 구현

--- ESC/src/main/java/com/smhrd/controller/ResultController.java
+++ ESC/src/main/java/com/smhrd/controller/ResultController.java
@@ -1,9 +1,10 @@
 package com.smhrd.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,15 +20,18 @@ import lombok.RequiredArgsConstructor;
 public class ResultController {
 	
 	private final ResultService resultService;
+	private final HttpSession httpSession;
 	
-	@GetMapping("/save/{user}")
-	public String rsSave(Model model, 
-			@PathVariable("user") User user,
+	@GetMapping("/save")
+	public String rsSave(Model model,
 			@RequestParam("roily") Long roily, 
 			@RequestParam("rresistant") Long rresistant,
 			@RequestParam("rnonPigment") Long rnonPigment,
 			@RequestParam("rtight") Long rtight){
 			// r_date, r_seq 는 자동시퀀스
+		
+		User user = (User) httpSession.getAttribute("user");
+		System.out.println(user.getUserNick());
 		
 		// ORNT
 		String OD = "D";
